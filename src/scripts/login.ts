b6be4cd564eed2081b8161991cd3a0d2599f758b
LukasJarolim <lukasjarolim01@seznam.cz>
2026-02-04 12:49:43 +0100

    Working on login.html

---
+++ src/scripts/login.ts
@@ -0,0 +1,5 @@
+const loginInput =  document.getElementById("login") as HTMLInputElement
+const passwordInput = document.getElementById("password") as HTMLInputElement
+document.getElementById("loginBtn")?.addEventListener("click",async ()=>{
+    fetch("")
+})
